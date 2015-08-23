59ad626250f92a8d352a0a051723f587c928b4a9
Kasey McCormick <mccormick.kasey@gmail.com>
2015-08-23 06:00:36 -0500

    adding columns to leads table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150819204300) do
+ActiveRecord::Schema.define(version: 20150823105856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,9 @@ ActiveRecord::Schema.define(version: 20150819204300) do
     t.integer  "wage"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.boolean  "internship"
+    t.string   "contact_person"
+    t.string   "contact_email"
   end
 
 end
